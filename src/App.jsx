faa5915c7270e5d7796f1b8ab50afadbe27a24e1
Mownica_webdev <mownica.p@gmail.com>
2024-11-20 22:46:56 +0530

    material-tailwind css Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@material-tailwind/react";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
     </div>
   );
 }

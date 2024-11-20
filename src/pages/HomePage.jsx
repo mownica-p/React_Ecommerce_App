643d357bf8493de37646399968d8974286f2fd48
Mownica_webdev <mownica.p@gmail.com>
2024-11-21 00:43:17 +0530

    created Testimonial component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import Category from "../components/category/Category";
 import HeroSection from "../components/heroSection/HeroSection";
 import HomePageProductCard from "../components/homePageProductCard/HomePageProductCard";
 import Layout from "../components/Layout";
+import Testimonial from "../components/testimonial/Testimonial";
 import Track from "../components/track/Track";
 
 function HomePage() {
@@ -11,6 +12,7 @@ function HomePage() {
       <Category />
       <HomePageProductCard />
       <Track />
+      <Testimonial />
     </Layout>
   );
 }

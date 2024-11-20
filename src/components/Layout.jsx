d9c6064a96b72112b1a732839abea466a2116b36
Mownica_webdev <mownica.p@gmail.com>
2024-11-20 23:32:58 +0530

    SSearchBar and Navbar code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,14 @@
-function Layout() {
-  return <div>Layout</div>;
-}
+/* eslint-disable react/prop-types */
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <div className="main-content min-h-screen">{children}</div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+function HomePage() {
+  return (
+    <Layout>
+      <h1>Welcome to the Homepage</h1>
+    </Layout>
+  );
+}
+
+export default HomePage;

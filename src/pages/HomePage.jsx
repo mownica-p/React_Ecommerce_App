547e16b1a3256472a57df8748ef0d6a78ef300de
Mownica_webdev <mownica.p@gmail.com>
2024-11-21 00:02:38 +0530

    create category component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Category from "../components/category/Category";
 import HeroSection from "../components/heroSection/HeroSection";
 import Layout from "../components/Layout";
 
@@ -5,6 +6,7 @@ function HomePage() {
   return (
     <Layout>
       <HeroSection />
+      <Category />
     </Layout>
   );
 }

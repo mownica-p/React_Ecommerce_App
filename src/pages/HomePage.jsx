f7776660e795a3a48a965ae3f4f20af3ca26dd98
Mownica_webdev <mownica.p@gmail.com>
2024-11-21 00:09:24 +0530

    created HomePageProductCard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Category from "../components/category/Category";
 import HeroSection from "../components/heroSection/HeroSection";
+import HomePageProductCard from "../components/homePageProductCard/HomePageProductCard";
 import Layout from "../components/Layout";
 
 function HomePage() {
@@ -7,6 +8,7 @@ function HomePage() {
     <Layout>
       <HeroSection />
       <Category />
+      <HomePageProductCard />
     </Layout>
   );
 }

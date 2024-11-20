76b626a3b9f3f12bffcd7109d629d762f3be0c0f
Mownica_webdev <mownica.p@gmail.com>
2024-11-21 01:33:52 +0530

    createdProductInfo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoPage from "./pages/NoPage";
 import HomePage from "./pages/HomePage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
 
 function App() {
   return (
     <div>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo" element={<ProductInfo />} />
         </Routes>
       </Router>
     </div>

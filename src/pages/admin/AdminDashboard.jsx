ce8acac2dcdee3f8242fe524350317061678fce7
Mownica_webdev <mownica.p@gmail.com>
2024-11-21 23:22:49 +0530

    updated Admin Dashboard Page

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,6 +4,7 @@ import OrderDetail from "./OrderDetail";
 import UserDetail from "./UserDetail";
 
 const AdminDashboard = () => {
+  const user = JSON.parse(localStorage.getItem("users"));
   return (
     <div>
       {/* Top */}
@@ -29,11 +30,24 @@ const AdminDashboard = () => {
             </div>
             {/* text  */}
             <div className="">
+              {/* Name  */}
               <h1 className=" text-center text-lg text-pink-500">
-                <span className=" font-bold">Name :</span> Kamal Nayan Upadhyay
+                <span className=" font-bold">Name :</span>
+                {user?.name}
               </h1>
+              {/* Email  */}
               <h1 className=" text-center text-lg text-pink-500">
-                <span className=" font-bold">Email :</span> test@gmail.com
+                <span className=" font-bold">Email :</span> {user?.email}
+              </h1>
+              {/* Date  */}
+              <h1 className=" text-center text-lg text-pink-500">
+                <span className=" font-bold">Date : </span>
+                {user?.date}
+              </h1>
+              {/* Role  */}
+              <h1 className=" text-center text-lg text-pink-500">
+                <span className=" font-bold">Role : </span>
+                {user?.role}
               </h1>
             </div>
           </div>

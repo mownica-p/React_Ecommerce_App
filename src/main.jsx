960fb51eb35717195492c4dfdcc1d97d4995f2f7
Mownica_webdev <mownica.p@gmail.com>
2024-11-22 17:22:46 +0530

    Redux ToolKit Setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
